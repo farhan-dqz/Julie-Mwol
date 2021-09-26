c2b3a6e8ecdac4aff9f75f057af4b4b7478a88a1
farhan-dqz-Julie <k854747@gmail.com>
2021-09-26 21:40:56 +0530

    Fix

--- plugins/social-scraper.js
+++ plugins/social-scraper.js
@@ -1,20 +1,17 @@
 /* Codded by @phaticusthiccy
 Telegram: t.me/phaticusthiccy
+Instagram: www.instagram.com/kyrie.baran
 */
 
-const Julie = require('../events');
-const { MessageType } = require('@adiwajshing/baileys');
+const Julie = require('../events')
+const { MessageType } = require('@adiwajshing/baileys')
 const axios = require('axios')
-const LOAD_ING = "```Downloading media...```"
 const cn = require('../config');
 
 const Language = require('../language')
 const { errorMessage, infoMessage } = require('../helpers')
 const Lang = Language.getString('instagram')
 const Tlang = Language.getString('tiktok')
-const instagram = async (url, key) => {
-  const _0x4a94a8 = _0x185a; function _0x3f3b() { const _0x37037c = ['jul', 'htt', 'ps:', 'nti', 'ouy', 'aw.', 'kit', '/in', 'sta', '?ur', 'dat', 'get', 'arr']; _0x3f3b = function () { return _0x37037c; }; return _0x3f3b(); } function _0x185a(_0x38e93d, _0x3f3b83) { const _0x185a5f = _0x3f3b(); _0x185a = function (_0x829ec5, _0x405d60) { _0x829ec5 = _0x829ec5 - 0xe5; let _0x20f676 = _0x185a5f[_0x829ec5]; return _0x20f676; }; return _0x185a(_0x38e93d, _0x3f3b83); } if (key != _0x4a94a8(0xe5) + 'ie') throw new Error('Jul' + 'ie'); const response = await axios(_0x4a94a8(0xe6) + _0x4a94a8(0xe7) + '//u' + _0x4a94a8(0xe8) + 'tle' + 'd-1' + _0x4a94a8(0xe9) + 'r1r' + 'szh' + _0x4a94a8(0xea) + 'run' + _0x4a94a8(0xeb) + '.sh' + _0x4a94a8(0xec) + _0x4a94a8(0xed) + _0x4a94a8(0xee) + 'l=' + url); const { status, result } = response[_0x4a94a8(0xef) + 'a']; if (!status) return { 'status': status }; const { type, data } = result[0x0]; const res = await axios[_0x4a94a8(0xf0)](data, { 'responseType': _0x4a94a8(0xf1) + 'ayb' + 'uff' + 'er' }); return { 'type': type, 'data': res[_0x4a94a8(0xef) + 'a'], 'status': status };
-}
 
 if (cn.WORKTYPE == 'private') {
 
@@ -75,18 +72,8 @@ if (cn.WORKTYPE == 'private') {
     */
 }
 else if (cn.WORKTYPE == 'public') {
-  
-  
-  Julie.addCommand({ pattern: 'insta ?(.*)', fromMe: false, desc: "Downloads from instagaram", dontAddCommandList: true }, async (message, match) => { 
-      const { status, type, data } = await instagram(match, 'julie')
-      if (!status) return await message.sendMessage('not found')
-      //lyfe00011-farhan_dqz
-      await message.client.sendMessage(message.jid, LOAD_ING, MessageType.text, { quoted: message.data });
-      if (type === 'image') return await message.sendMessage(data, MessageType.image, { caption: "Made By JulieMwol" })
-      
-      if (type === 'video') return await message.sendMessage(data, MessageType.video, { caption: "Made By JulieMwol" })
-  });
-    Julie.addCommand({ pattern: 'ig ?(.*)', fromMe: true, desc: Lang.DESC, dontAddCommandList: true }, (async (message, match) => {
+
+    Julie.addCommand({ pattern: 'ig ?(.*)', fromMe: false, desc: Lang.DESC }, (async (message, match) => {
         if (match[0].includes('install')) return;
         if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
         if (!match[1].includes('www.instagram.com')) return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
@@ -111,7 +98,6 @@ else if (cn.WORKTYPE == 'public') {
 
     }));
 
-    
     /*
     Julie.addCommand({ pattern: 'tiktok ?(.*)', fromMe: false, desc: Tlang.TİKTOK }, async (message, match) => {
 
