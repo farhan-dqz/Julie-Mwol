9b582f3b3fc2393eea110a960d63006718a08577
Ravana tech <85940832+Thisan01@users.noreply.github.com>
2021-10-17 08:15:07 +0530

    Update spdf.js

--- plugins/spdf.js
+++ plugins/spdf.js
@@ -9,14 +9,14 @@ let Language = require('../language');
 let Lang = Language.getString('unvoice');
 let toxicdevil = Config.WORKTYPE == 'public' ? false : true
 
-   Julie.addCommand({pattern: 'spdf ?(.*)', fromMe: toxicdevil, desc: 'Converts Site to PDF.' }, (async (message, match) => {
+   Julie.addCommand({pattern: 'spdf ?(.*)', fromMe: toxicdevil, desc: 'පිටුව PDF ලෙස සකසමින්🌠.' }, (async (message, match) => {
 
-    if (match[1] === '') return await message.client.sendMessage(message.jid, '*You must enter a link!*', MessageType.text);
+    if (match[1] === '') return await message.client.sendMessage(message.jid, '*ඔබ අනිවාර්යයෙන් ලින්ක් එකක් ඇතුළත් කල යුතුයි!*', MessageType.text);
 
     var webimage = await axios.get(`https://api.html2pdf.app/v1/generate?url=${match[1]}&apiKey=begC4dFAup1b8LyRXxAfjetfqDg2uYx8PWmh9YJ59tTZXiUyh2Vs72HdYQB68vyc`, { responseType: 'arraybuffer' })
 
-    await message.client.sendMessage(message.jid, '```Converting Site to PDF```', MessageType.text);
+    await message.client.sendMessage(message.jid, '```පිටුව PDF ලෙස සකසමින්🌠```', MessageType.text);
 
-    await message.sendMessage(Buffer.from(webimage.data), MessageType.document, {mimetype: Mimetype.pdf, filename: 'Julie-Mwol.pdf'});
+    await message.sendMessage(Buffer.from(webimage.data), MessageType.document, {mimetype: Mimetype.pdf, filename: 'devil👨‍💻.pdf'});
 
     }));    
