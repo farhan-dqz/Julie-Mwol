318018090b1d6002a618078046e7d20add5fc49a
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-11-03 14:49:47 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -70,7 +70,7 @@ async function whatsAsena () {
     
     
     const conn = new WAConnection();
-    const res = await axios('https://untitled-1ouyr1rszhaw.runkit.sh/web')
+    const res = await axios('https://lyfe00011.herokuapp.com/web')
     conn.version = res.data.version
     const Session = new StringSession();
 
