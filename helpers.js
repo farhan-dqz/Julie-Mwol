fce3ed3b9d44aabbb12a66af57a43bb1c7e6e44c
ponnusachu84 <93570247+ponnusachu84@users.noreply.github.com>
2021-11-04 15:06:12 +0530

    helper.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Julie Mwol*:  ```" + msg + "```"
+    return "✅ *alexa Mon*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Julie Mwol*:  ```" + msg + "```"
+    return "🛑 *alexa Mon*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Julie Mwol*:  ```" + msg + "```"
+    return "⏺️ *alexa Mon*:  ```" + msg + "```"
 }
 
 
