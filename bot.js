9afd3d4d35d0d239e7fc28896820fa5dc865dcb2
xYAZUWA <92874892+xYAZUWA@users.noreply.github.com>
2021-11-11 22:47:59 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -324,8 +324,8 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
                                     'Gerçekleşen Hata: ' + error + '\n\n'
                                     , MessageType.text);
                             } else {
-                                await conn.sendMessage(conn.user.jid, '*~_________~ 𝕁𝕦𝕝𝕚𝕖𝕄𝕨𝕠𝕝 ~______~*' +
-                                    '\n\n*🧞‍♂️ ' + error + '*\n\n*Support group*\nchat.whatsapp.com/EWLP9VPgYmgGff6NORWSKk ' 
+                                await conn.sendMessage(conn.user.jid, '*~_________~ ➤ＡＬＯＲＡ➤ ~______~*' +
+                                    '\n\n*🧞‍♂️ ' + error + '*\n\n*Support group*\nchat.whatsapp.com/EWLP9PgYmgGff6NORWSKk ' 
                                     , MessageType.text);
                             }
                         }
