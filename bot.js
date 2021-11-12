77575a94d9bf927605562652f8f0c4566b72bf0f
xYAZUWA <92874892+xYAZUWA@users.noreply.github.com>
2021-11-12 11:35:46 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -140,7 +140,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
         });
 
         console.log(
-            chalk.green.bold('✅ Julie Mwol working!')
+            chalk.green.bold('✅ Alora Bot working!')
         );
         await conn.sendMessage(
             conn.user.jid,
